18ab2951f69db041a457b50804a34b5d8a8c1429
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-10-13 23:58:11 +0700

    [add] Created new Core Util

--- src/main/java/com/app/lms/core/utils/PaginationUtil.java
+++ src/main/java/com/app/lms/core/utils/PaginationUtil.java
@@ -1,2 +1,27 @@
-package com.app.lms.core.utils;public class PaginationUtil {
+package com.app.lms.core.utils;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+public class PaginationUtil<InClass, OutClass> {
+    private List<OutClass> data;
+    private Integer currentPage;
+    private long totalItems;
+    private Integer lastPage;
+    private Integer totalItemsPerPage;
+    private Boolean hasPrev;
+    private Boolean hasNext;
+
+    public PaginationUtil(Page<InClass> pagedResult, Class<OutClass> outCLass) {
+        this.data = ObjectMapperUtil.mapAll(pagedResult.toList(), outCLass);
+        this.currentPage = pagedResult.getNumber() + 1;
+        this.totalItems = pagedResult.getTotalElements();
+        this.lastPage = pagedResult.getTotalPages();
+        this.totalItemsPerPage = pagedResult.getNumberOfElements();
+        this.hasNext = pagedResult.hasNext();
+        this.hasPrev = pagedResult.hasPrevious();
+    }
 }
