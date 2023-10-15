3b1fff4611f6dc0013f93e2bea2a586470960c4c
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-10-15 11:00:20 +0700

    [add] created new validation to validate date format

--- src/main/java/com/app/lms/core/validations/IsDate.java
+++ src/main/java/com/app/lms/core/validations/IsDate.java
@@ -1,2 +1,19 @@
-package com.app.lms.core.validations;public class IsDate {
+package com.app.lms.core.validations;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = IsDateValidator.class)
+public @interface IsDate {
+
+    public String message() default "Invalid Date";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
 }
