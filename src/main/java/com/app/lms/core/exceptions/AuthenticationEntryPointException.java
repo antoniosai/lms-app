04d3c3302fa8d5968ca7a378868de084eb94400d
Antonio Saiful Islam <finallyantonio@gmail.com>
2023-10-22 01:12:14 +0700

    [update] added new exception to handle AccessDenied

--- src/main/java/com/app/lms/core/exceptions/AuthenticationEntryPointException.java
+++ src/main/java/com/app/lms/core/exceptions/AuthenticationEntryPointException.java
@@ -1,4 +1,18 @@
 package com.app.lms.core.exceptions;
 
-public class AuthenticationEntryPointException {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class AuthenticationEntryPointException implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.addHeader("access_denied_reason", "authentication_required");
+        response.sendError(403, "Access Denied");
+    }
 }
